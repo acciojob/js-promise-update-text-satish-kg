1f2682cd2740f69346c4fab001b64a394ed26d01
Satish Kr <satishkrgupta555@gmail.com>
2023-06-03 20:28:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function test() {
+	let prom = new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!")
+		}, 1000);
+	});
+	prom.then((data)=>{
+		let ele = document.getElementById("output");
+		ele.innerText = data;
+	})
+}
+test();
